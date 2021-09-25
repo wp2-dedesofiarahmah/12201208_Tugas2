1658f206e6dbdf426b8d4f29e577cf1c38548234
wp2-dedesofiarahmah <dedesofiar@gmail.com>
2021-09-25 12:43:26 +0700

    menambahkan framework

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya Dede Sofia Rahmah
+            Alamat saya di Jl. gadog RT 05 RW 11
+            No Telpon saya 083826161364
+            Olahraga yang disukai adalah Renang";
+    }
+}
+?>
